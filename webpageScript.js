f42d5fe0ccab1e7666fdd5e048f60eac479231cc
Ethan Printz <ethanprintz@gmail.com>
2021-07-09 14:35:20 -0400

    Added Option 2

--- webpageScript.js
+++ webpageScript.js
@@ -1,17 +1,17 @@
-/*explore drop down onclick*/
-function exploreDropDown() {
-  document.getElementById('dropdown').classList.toggle('show');
-}
-/*close drop down when clicked other places*/
-window.onclick = function (event) {
-  if (!event.target.matches('#explore-button')) {
-    var dropdowns = document.getElementsByClassName('dropdown-content');
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-};
+// /*explore drop down onclick*/
+// function exploreDropDown() {
+//   document.getElementById('dropdown').classList.toggle('show');
+// }
+// /*close drop down when clicked other places*/
+// window.onclick = function (event) {
+//   if (!event.target.matches('#explore-button')) {
+//     var dropdowns = document.getElementsByClassName('dropdown-content');
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// };
